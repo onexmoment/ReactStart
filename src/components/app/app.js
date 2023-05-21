6945d881df89b7874f5cf69c45c9f42d985c7633
Aleksey <example@gmail.com>
2023-05-21 12:33:30 +0300

    adding functionality for adding and removing employees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,28 +8,61 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data : [
+				{name: "John C.", salary: 1000, increase: false, id: 1},
+				{name: "Alex M.", salary: 800, increase: true, id: 2},
+				{name: "Craig R", salary: 3000, increase: false, id: 3}
+			]
+		}
 
-	const data = [
-		{name: "John C.", salary: 1000, increase: false, id: 1},
-		{name: "Alex M.", salary: 800, increase: true, id: 2},
-		{name: "Craig R", salary: 3000, increase: false, id: 3}
-	]
+		this.maxId = 4;
+	}
 
-	return (
-		<div className="app">
-			<AppInfo/>
+	deleteItem = (id) => {
+		this.setState(({data}) => {
+			return {
+				data: data.filter(item => item.id !== id)
+			}
+		})
+	}
 
-			<div className="search-panel">
-				<SearchPanel/>
-				<AppFilter/>
+	addItem = (name, salary) => {
+		const newItem = {
+			name: name,
+			salary: salary,
+			increase: false,
+			id: this.maxId++
+		}
+
+		this.setState(({data}) => {
+			return {
+				data: [...data, newItem]
+			}
+		})
+	}
+
+	render() {
+		return (
+			<div className="app">
+				<AppInfo/>
+	
+				<div className="search-panel">
+					<SearchPanel/>
+					<AppFilter/>
+				</div>
+	
+				<EmployeesList
+				data={this.state.data}
+				onDelete={this.deleteItem}/>
+	
+				<EmployeesAddForm onAdd={this.addItem}/>
 			</div>
-
-			<EmployeesList data={data}/>
-
-			<EmployeesAddForm/>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
